d265a38aec49165b4b81c7a63e87fee511f70907
Jonathan Corredor Oficina <jhoon.neo@gmail.com>
2017-08-14 18:28:48 -0500

    cambio de nombre

---
+++ collect_app/src/main/java/org/co2arauca/collect/android/activities/AppListActivity.java
@@ -0,0 +1,361 @@
+/*
+ * Copyright 2017 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.co2arauca.collect.android.activities;
+
+import android.content.res.Configuration;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import org.co2arauca.collect.android.R;
+import org.co2arauca.collect.android.application.Collect;
+import org.co2arauca.collect.android.database.ActivityLogger;
+import org.co2arauca.collect.android.provider.InstanceProviderAPI;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import static org.co2arauca.collect.android.utilities.ApplicationConstants.SortingOrder.BY_NAME_ASC;
+
+abstract class AppListActivity extends AppCompatActivity {
+    protected final ActivityLogger logger = Collect.getInstance().getActivityLogger();
+
+    private static final String SELECTED_INSTANCES = "selectedInstances";
+    private static final String IS_SEARCH_BOX_SHOWN = "isSearchBoxShown";
+
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private LinearLayout searchBoxLayout;
+    private EditText inputSearch;
+
+    protected SimpleCursorAdapter listAdapter;
+    protected LinkedHashSet<Long> selectedInstances = new LinkedHashSet<>();
+    protected String[] sortingOptions;
+
+    private boolean isSearchBoxShown;
+
+    protected Integer selectedSortingOrder;
+    protected Toolbar toolbar;
+    protected ListView listView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        listView = (ListView) findViewById(android.R.id.list);
+        listView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+
+        TextView emptyView = (TextView) findViewById(android.R.id.empty);
+        listView.setEmptyView(emptyView);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        searchBoxLayout = (LinearLayout) findViewById(R.id.searchBoxLayout);
+        restoreSelectedSortingOrder();
+        setupSearchBox();
+        setupDrawer();
+        setupDrawerItems();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(SELECTED_INSTANCES, selectedInstances);
+        outState.putBoolean(IS_SEARCH_BOX_SHOWN, searchBoxLayout.getVisibility() == View.VISIBLE);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+        selectedInstances = (LinkedHashSet<Long>) state.getSerializable(SELECTED_INSTANCES);
+        isSearchBoxShown = state.getBoolean(IS_SEARCH_BOX_SHOWN);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Collect.getInstance().getActivityLogger().logInstanceAction(this, "onCreateOptionsMenu", "show");
+        getMenuInflater().inflate(R.menu.list_menu, menu);
+
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_sort:
+                if (drawerLayout.isDrawerOpen(Gravity.END)) {
+                    drawerLayout.closeDrawer(Gravity.END);
+                } else {
+                    Collect.getInstance().hideKeyboard(inputSearch);
+                    drawerLayout.openDrawer(Gravity.END);
+                }
+                return true;
+
+            case R.id.menu_filter:
+                if (searchBoxLayout.getVisibility() == View.GONE) {
+                    showSearchBox();
+                } else {
+                    hideSearchBox();
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if (drawerToggle != null) {
+            drawerToggle.syncState();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (drawerToggle != null) {
+            drawerToggle.onConfigurationChanged(newConfig);
+        }
+    }
+
+    private void setupSearchBox() {
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateAdapter();
+            }
+        });
+
+        if (isSearchBoxShown) {
+            showSearchBox();
+            updateAdapter();
+        }
+    }
+
+    private void hideSearchBox() {
+        inputSearch.setText("");
+        searchBoxLayout.setVisibility(View.GONE);
+        Collect.getInstance().hideKeyboard(inputSearch);
+    }
+
+    private void showSearchBox() {
+        searchBoxLayout.setVisibility(View.VISIBLE);
+        Collect.getInstance().showKeyboard(inputSearch);
+    }
+
+    private void setupDrawerItems() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sortingOptions) {
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                if (position == getSelectedSortingOrder()) {
+                    textView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.tintColor));
+                }
+                textView.setPadding(50, 0, 0, 0);
+                return textView;
+            }
+        };
+        drawerList.setAdapter(adapter);
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                parent.getChildAt(selectedSortingOrder).setBackgroundColor(Color.TRANSPARENT);
+                view.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.tintColor));
+                performSelectedSearch(position);
+                drawerLayout.closeDrawer(Gravity.END);
+            }
+        });
+    }
+
+    private void performSelectedSearch(int position) {
+        saveSelectedSortingOrder(position);
+        updateAdapter();
+    }
+
+    private void setupDrawer() {
+        drawerList = (ListView) findViewById(R.id.sortingMenu);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.sorting_menu_open, R.string.sorting_menu_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                supportInvalidateOptionsMenu();
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                supportInvalidateOptionsMenu();
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            }
+        };
+
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(drawerToggle);
+    }
+
+    protected void checkPreviouslyCheckedItems() {
+        listView.clearChoices();
+        List<Integer> selectedPositions = new ArrayList<>();
+        int listViewPosition = 0;
+        Cursor cursor = listAdapter.getCursor();
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                long instanceId = cursor.getLong(cursor.getColumnIndex(InstanceProviderAPI.InstanceColumns._ID));
+                if (selectedInstances.contains(instanceId)) {
+                    selectedPositions.add(listViewPosition);
+                }
+                listViewPosition++;
+            } while (cursor.moveToNext());
+        }
+
+        for (int position : selectedPositions) {
+            listView.setItemChecked(position, true);
+        }
+    }
+
+    protected abstract void updateAdapter();
+
+    protected abstract String getSortingOrderKey();
+
+    protected boolean areCheckedItems() {
+        return getCheckedCount() > 0;
+    }
+
+    protected int getCheckedCount() {
+        return listView.getCheckedItemCount();
+    }
+
+    // toggles to all checked or all unchecked
+    // returns:
+    // true if result is all checked
+    // false if result is all unchecked
+    //
+    // Toggle behavior is as follows:
+    // if ANY items are unchecked, check them all
+    // if ALL items are checked, uncheck them all
+    public static boolean toggleChecked(ListView lv) {
+        // shortcut null case
+        if (lv == null) {
+            return false;
+        }
+
+        boolean newCheckState = lv.getCount() > lv.getCheckedItemCount();
+        setAllToCheckedState(lv, newCheckState);
+        return newCheckState;
+    }
+
+    public static void setAllToCheckedState(ListView lv, boolean check) {
+        // no-op if ListView null
+        if (lv == null) {
+            return;
+        }
+
+        for (int x = 0; x < lv.getCount(); x++) {
+            lv.setItemChecked(x, check);
+        }
+    }
+
+    // Function to toggle button label
+    public static void toggleButtonLabel(Button toggleButton, ListView lv) {
+        if (lv.getCheckedItemCount() != lv.getCount()) {
+            toggleButton.setText(R.string.select_all);
+        } else {
+            toggleButton.setText(R.string.clear_all);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(Gravity.END)) {
+            drawerLayout.closeDrawer(Gravity.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void saveSelectedSortingOrder(int selectedStringOrder) {
+        selectedSortingOrder = selectedStringOrder;
+        PreferenceManager.getDefaultSharedPreferences(Collect.getInstance())
+                .edit()
+                .putInt(getSortingOrderKey(), selectedStringOrder)
+                .apply();
+    }
+
+    protected void restoreSelectedSortingOrder() {
+        selectedSortingOrder = PreferenceManager
+                .getDefaultSharedPreferences(Collect.getInstance())
+                .getInt(getSortingOrderKey(), BY_NAME_ASC);
+    }
+
+    protected int getSelectedSortingOrder() {
+        if (selectedSortingOrder == null) {
+            restoreSelectedSortingOrder();
+        }
+        return selectedSortingOrder;
+    }
+
+    protected CharSequence getFilterText() {
+        return inputSearch != null ? inputSearch.getText() : "";
+    }
+}
